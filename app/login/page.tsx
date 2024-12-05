cc6fe31c0c6bed31e4309ca808130d868a4c6766
hangxigood <hangxigood@gmail.com>
2024-12-05 09:29:28 -0700

    Put 2 Credentials on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -48,7 +48,7 @@ export default function LoginPage() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Sign in to your account(admin/Admin!123 or user/User!123)
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{' '}
